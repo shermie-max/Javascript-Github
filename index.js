512ce8c097ea0aa47f56e6bf5fac787a9c0fa5d9
shermie-max <hcnp89@gmail.com>
2022-07-14 10:11:26 +0700

    JavaScript practice

---
+++ index.js
@@ -0,0 +1,28 @@
+const day = 4;
+
+switch (day) {
+    case 0:
+        console.log("Monday");
+        break;
+    case 1:
+        console.log("Tuesday");
+        break;
+    case 2:
+        console.log("Wednesday");
+        break;
+    case 3:
+        console.log("Thursday");
+        break;
+    case 4:
+        console.log("Friday");
+        break;
+    case 5:
+        console.log("Saturday");
+        break;
+    case 6:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Error");
+        break;
+}
